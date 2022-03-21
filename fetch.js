cc429eb35b7920abaf8a1a12925481d2809d9146
prodip763 <prodip12763@gmail.com>
2022-03-21 22:18:31 +0600

    first commit

---
+++ fetch.js
@@ -0,0 +1,12 @@
+console.log(111111);
+console.log(222222);
+// setTimeout(() => console.log('aaaaaaa'));
+setTimeout(() => console.log('aaaaaaa'), 7000);
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(json => console.log(json))
+console.log(333333);
+console.log(4444444);
+for (let i = 0; i < 40000; i++) {
+    console.log(i);
+}
---
+++ settimeout.js
@@ -0,0 +1,18 @@
+function doSomething() {
+    console.log('lot of loves all world');
+}
+console.log('prodip is the world largest businessmen');
+console.log('he loves a girl');
+// doSomething();
+// setTimeout(doSomething, 5000);
+// // setTimeout function ta funtion a sobar ses a bose and 5000 dia 5s bujaice 
+setTimeout(function() {
+    console.log('ore baba tui ki jinis re');
+}, 3000)
+setTimeout(() => {
+    console.log('okay , thank you');
+}, 2000)
+setTimeout(() => {
+    console.log('i love you');
+}, 2000)
+console.log('he loves her family so much');
